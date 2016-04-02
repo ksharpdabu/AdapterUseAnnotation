c435fb111c85de3f6256fd245415509ea9a17a48
ksharpdabu123@gmail.com <@git1qazxsw2>
2016-04-02 16:44:28 +0800

    开始写注解方式的适配器

--- app/src/main/java/info/dabu/adapteruseannotation/CustomeAdapter.java
+++ app/src/main/java/info/dabu/adapteruseannotation/CustomeAdapter.java
@@ -1,11 +1,17 @@
 package info.dabu.adapteruseannotation;
 
+import android.app.Activity;
+import android.support.v4.util.LongSparseArray;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 
+import java.util.List;
+
 import info.dabu.adapteruseannotation.Adapter.ImageDelegateAdapter;
 import info.dabu.adapteruseannotation.Adapter.TextDelegateAdapter;
+import info.dabu.adapteruseannotation.annotation.DelegateAdapterType;
 import info.dabu.adapteruseannotation.annotation.DelegateAdapters;
 
 /**
@@ -19,17 +25,34 @@ import info.dabu.adapteruseannotation.annotation.DelegateAdapters;
 })
 public class CustomeAdapter extends BaseAdapter {
 
+    private LongSparseArray<DelegateAdapter> mDelegateAdapterSparseArray;
 
 
+    private  Activity mActivity;
+
+
+    private LayoutInflater mLayoutInflater;
+
+
+    private List<ListViewItem> mList;
+
+
+    public CustomeAdapter(List<ListViewItem> list, Activity activity) {
+        mList = list;
+        mActivity = activity;
+
+
+        initDelegates();
+    }
 
     @Override
     public int getCount() {
-        return 0;
+        return mList.size();
     }
 
     @Override
     public Object getItem(int position) {
-        return null;
+        return mList.get(position);
     }
 
     @Override
@@ -37,8 +60,78 @@ public class CustomeAdapter extends BaseAdapter {
         return 0;
     }
 
+
+    @Override
+    public int getViewTypeCount() {
+        return mDelegateAdapterSparseArray.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        return mList.get(position).getType();
+    }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        long itemtype = getItemViewType(position);
+
+
+        DelegateAdapter delegateAdapter  = mDelegateAdapterSparseArray.get(itemtype);
+
+        if (delegateAdapter != null){
+            throw new RuntimeException("Unknown type "+itemtype+" called");
+        }
+
+        //返回每种适配器对应的View
+        return mDelegateAdapterSparseArray.get(itemtype).getView(position, convertView, parent, mLayoutInflater, getItem(position));
     }
+
+
+
+    private void initDelegates(){
+        mDelegateAdapterSparseArray = new LongSparseArray<DelegateAdapter>();
+
+        DelegateAdapters annotation = getClass().getAnnotation(DelegateAdapters.class);
+
+        if ( annotation != null){
+            Class[] clazzs = annotation.delegateAdapters();
+
+            for ( Class<?> clazz : clazzs){
+                DelegateAdapterType delegateAdapterAnnotation = clazz.getAnnotation(DelegateAdapterType.class);
+
+                if ( delegateAdapterAnnotation == null){
+                    throw new RuntimeException("The class "+clazz.getName()+" should have the annotation DelegateAdapterType");
+                }
+
+                long itemType = delegateAdapterAnnotation.itemType();
+
+                if ( mDelegateAdapterSparseArray.get(itemType) != null){
+                    throw new RuntimeException("The item type "+itemType+" is already defined!");
+                }
+
+                DelegateAdapter adapter  = null;
+
+                try {
+
+                    adapter = (DelegateAdapter) clazz.newInstance();
+                }catch (Exception e){
+
+                    throw  new RuntimeException("Error while instantiating "+clazz.getName()+" with default constructor: "+e.getMessage(), e);
+
+                }
+
+
+                mDelegateAdapterSparseArray.put(itemType, adapter);
+
+            }
+
+        }
+
+    }
+
+
+
+
+
 }
