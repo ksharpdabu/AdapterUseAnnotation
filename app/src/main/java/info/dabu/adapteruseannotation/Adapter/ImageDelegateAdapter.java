fb720c8586e225ba34d1cc4c121eb055410c241c
ksharpdabu123@gmail.com <@git1qazxsw2>
2016-04-02 16:53:13 +0800

    开始写注解方式的适配器

--- app/src/main/java/info/dabu/adapteruseannotation/Adapter/ImageDelegateAdapter.java
+++ app/src/main/java/info/dabu/adapteruseannotation/Adapter/ImageDelegateAdapter.java
@@ -3,8 +3,10 @@ package info.dabu.adapteruseannotation.Adapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import info.dabu.adapteruseannotation.DelegateAdapter;
+import info.dabu.adapteruseannotation.R;
 import info.dabu.adapteruseannotation.annotation.DelegateAdapterType;
 
 /**
@@ -16,6 +18,41 @@ public class ImageDelegateAdapter implements DelegateAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent, LayoutInflater inflater, Object item) {
-        return null;
+
+        final  ImageViewHolder imageViewHolder;
+
+
+        if ( convertView == null){
+            convertView =  inflater.inflate(R.layout.type_imageview,parent,false);
+
+            imageViewHolder = new ImageViewHolder();
+            imageViewHolder.imageView = (ImageView) convertView.findViewById(R.id.image);
+
+            convertView.setTag(imageViewHolder);
+        }else {
+            imageViewHolder = (ImageViewHolder) convertView.getTag();
+        }
+
+
+        if ( null != imageViewHolder.imageView){
+            imageViewHolder.imageView.setImageResource(R.mipmap.bangalore);
+        }
+
+
+
+        return  convertView;
+
+
+
+
     }
+
+
+
+    //    用来优化含ImageView的布局
+    public class ImageViewHolder {
+        ImageView imageView;
+
+    }
+
 }
